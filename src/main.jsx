ec64655273ba5d2ef4592cb22bce071a95aa0d6d
Jack Everitt <jackeveritt9@gmail.com>
2026-06-02 13:45:45 +0000

    V 0.3.5.2

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+
 import App from './App';
 import './styles/globals.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import {
+  AppProvider,
+} from './context/AppContext';
+
+ReactDOM.createRoot(
+  document.getElementById('root')
+).render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <AppProvider>
+        <App />
+      </AppProvider>
     </HashRouter>
   </React.StrictMode>
 );
