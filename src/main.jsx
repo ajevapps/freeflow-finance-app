bbaa937e6f8e8b309c3404fce58b2b116e0d34f9
Jack Everitt <jackeveritt9@gmail.com>
2026-06-02 13:38:56 +0000

    V 0.3.5

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './styles/globals.css';
 
-import {
-  AppProvider,
-} from './context/AppContext';
-
-ReactDOM.createRoot(
-  document.getElementById(
-    'root'
-  )
-).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </BrowserRouter>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
